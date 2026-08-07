42e42ecc14a92f019bf45c14e24965e1d61cc2f2
suu-b <thakur0805@gmail.com>
2026-08-07 11:37:48 +0530

    feat: orchestrator

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
 # Orchestrator script to run the pipeline
 # Upload CSV/JSON file -> Clean Data -> Run Validation -> Run analytics -> Report using plotly and streamlit UI
 
+import argparse
+import logging
+
+# Required Models
 from shared.schemas.upload_request import UploadRequest
 from shared.schemas.dataset import Dataset
-from shared.schemas.validation_rule import ValidationRule
 
-# Run pipeline from here
+# Services
+from ingestion.main import IngestionService
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Run the data pipeline")
+    parser.add_argument("file_path", type=str, help="Path to the CSV/JSON file")
+    args = parser.parse_args()
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+    logger = logging.getLogger(__name__)
+    ingestion_service = IngestionService()
+    
+    file_path = args.file_path
+    upload_request: UploadRequest = UploadRequest(file_path=file_path)
+
+    dataset: Dataset = ingestion_service.ingest(file_path)
+    logger.info(dataset) 
+    
