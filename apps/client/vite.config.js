b78d3800743f5174e83417fe9d0360952d205ea8
suu-b <thakur0805@gmail.com>
2026-08-24 13:14:15 +0530

    chore: deploy apps

--- apps/client/vite.config.js
+++ apps/client/vite.config.js
@@ -5,4 +5,17 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+      }
+    }
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  }
 })
